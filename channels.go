a212a96eeff32e47404e33cc158aa55625a6de0e
meteoroid <meteoroid@zoho.com>
2014-08-29 15:36:08 -0500

    Add time channel

--- channels.go
+++ channels.go
@@ -1,7 +1,17 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+)
 
 func main() {
-	fmt.Printf("channels\n")
+	timeChan := make(chan time.Time)
+	go func() {
+		time.Sleep(time.Second)
+		timeChan <- time.Now()
+	}()
+	fmt.Println("waiting for chan...")
+	completedAt := <-timeChan
+	fmt.Println("completedAt:", completedAt)
 }
